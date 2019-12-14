53b7b25ce8abe22cff513b2dc72f1ddc9d98db55
Avery-Desktop <averyferrante123@gmail.com>
2019-12-14 13:29:41 -0600

    Added AngularFire + configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCSZKNtF5ThBPmBaTKU5j1kQ1fPvQzJZNQ',
+    authDomain: 'fireside-chat-ffe16.firebaseapp.com',
+    databaseURL: 'https://fireside-chat-ffe16.firebaseio.com',
+    projectId: 'fireside-chat-ffe16',
+    storageBucket: 'fireside-chat-ffe16.appspot.com',
+    messagingSenderId: '357240222565',
+    appId: '1:357240222565:web:62a047fbfec18bb03aa78c',
+    measurementId: 'G-YXNDBQ6NQ8'
+  }
 };
 
 /*
